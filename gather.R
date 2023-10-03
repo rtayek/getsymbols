5be2c952eba1487cdbdf6f020187efc342808cb2
rtayek <rtayek@gmail.com>
2023-10-03 06:14:32 -0700

    added repo on github.

--- gather.R
+++ gather.R
@@ -6,18 +6,16 @@ df<-NULL
 outputFile<-file.path("data", "highest.csv",  fsep = "\\")
 for(file in files) {
     if(startsWith(file,"bankroll")) {
-        print(file)
         p<-file.path("data", file,  fsep = "\\")
         x<-read.csv(p)
-        print(nrow(x))
         if(!once) {
             df<-x[0,]
             once<-TRUE
         }
         df<-rbind(df,x)
     }
-    print(nrow(df))
 }
+print(nrow(df))
 sorted <- df[order(-df$bankroll),]
 print(head(sorted))
 write.csv(sorted,file=outputFile,row.names=FALSE)
