1873e421bb1a823019ffd9944f0bd1ec4f546c5c
rtayek <rtayek@gmail.com>
2023-11-05 14:07:48 -0800

    read java nwo writes someunique.csv.

--- readjava.R
+++ readjava.R
@@ -8,12 +8,11 @@ getJava <- function() {
 y <- getYahooStocks()
 j<-getJava()
 df<-y[0,]
-stocks<-y[0,] # gasther unique symbols
-for(i in 1:5) {
-    print("------------")
+unique<-y[0,] # gasther unique symbols
+for(i in 1:nrow(j)) {
     ticker<-j$ticker[i]
     ticker<-trimws(ticker)
-    print(sprintf("ticker: '%s'",ticker))
+    #print(sprintf("ticker: '%s'",ticker))
     w<-which(y$Ticker==ticker)
     if(length(w)==0) {
         print("length is 0")
@@ -22,16 +21,22 @@ for(i in 1:5) {
     stock<-y[w,]
     df[nrow(df) + 1, ] <- stock
     #print(sprintf("which: '%s'",w))
-    dup<-which(stocks$Ticker==ticker) # unique?
-    print(sprintf("unique: '%s'",any(dup)))
-    print(dup)
+    dup<-which(unique$Ticker==ticker) # unique?
+    #print(sprintf("unique: '%s'",any(dup)))
+    #print(dup)
     if(!any(dup)) {
-        stocks[nrow(stocks) + 1, ] <- stock
+        print(sprintf("adding : '%s'",ticker))
+        unique[nrow(unique) + 1, ] <- stock
     }
     #print(stock)
 }
 nrow(df)
-nrow(stocks)
+nrow(unique)
+head(unique)
+sorted<-unique[order(unique$Exchange,unique$Ticker),]
+head(sorted)
+write.csv(sorted,file="someunique.csv", row.names = FALSE)
+
 
 
 
